037635369db092c8a526231fc5d954dc77dacf46
SalmanGits <belalahmad10310@gmail.com>
2023-07-06 22:03:56 +0530

    add and get user profile

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const {loginUser,registerUser} = require('../controller/UserController')
+const {loginUser,registerUser, addUserInfo, getUserInfo} = require('../controller/UserController')
+const Auth = require('../auth/auth')
 
 
 
 router.post("/login",loginUser)
 router.post("/register",registerUser)
+router.post("/edit-profile",Auth,addUserInfo)
+router.post("/user-profile",Auth,getUserInfo)
 
 
 
